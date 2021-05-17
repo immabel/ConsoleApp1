eba690fc88ce7ffc6687a40e8ba0af0dd3690242
immabel <maryna.tymchenko@nure.ua>
2021-05-17 22:10:02 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using MatrixClassLibrary;
+using System;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat = new int[,]
+            {
+                { 4, 7, 6 },
+                { 2, 15, 9 },
+                { 3, 5, 1 }
+            };
+
+            Matrix matrix = new Matrix(mat);
+
+            Console.WriteLine("Print: ");
+            matrix.Print();
+
+            Assembly assembly = Assembly.LoadFrom("MatrixClassLibrary.dll");
+            Console.WriteLine(assembly.FullName);
+            Type[] types = assembly.GetTypes();
+            foreach (Type t in types)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+            Type myType = Type.GetType("MatrixClassLibrary.Matrix, MatrixClassLibrary", false, true);
+            foreach (var mi in myType?.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+            }
+
+            Console.Read();
+        }
+    }
+}
